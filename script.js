364a91f1dce4e9b091b7fd8336b55cab0eb207d8
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-19 15:34:22 +0200

    feat(script): Second step of the project
    Added the hover casual color function

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector(".container");
 
+function casualColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return color = `rgb(${red}, ${green}, ${blue})`;
+}
+
 let l = container.offsetWidth / 16;
 
 for (let i = 1; i < 16 ** 2 + 1; i++) {
