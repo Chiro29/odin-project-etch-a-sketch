13ef8f3c854a25565cbc339260e1f2e16fc2f3f8
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-19 15:50:03 +0200

    feat(project): completed step 3 of the project:
    Added a button that allow you to select a number for the number of squares.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 
 function casualColor() {
@@ -8,17 +9,34 @@ function casualColor() {
   return color = `rgb(${red}, ${green}, ${blue})`;
 }
 
-let l = container.offsetWidth / 16;
-
-for (let i = 1; i < 16 ** 2 + 1; i++) {
-  const square = document.createElement("div");  
-  square.classList.add("grid");
-  square.style.width = l + "px";
-  square.style.height = l + "px";
-  container.appendChild(square);
-
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = casualColor();
-  });
+function gridSquare(a) {
+  
 }
+ 
+btn.addEventListener("click", () => {
+  const squaresToRemove = document.querySelectorAll(".grid");
+  let n = 0;
+  
+  do {
+    n = prompt("Choose a number between 1 to 100");
+    n = parseInt(n);
+  } while (isNaN(n) || n < 1 || n > 100);
 
+  if (squaresToRemove.length > 0) {
+    squaresToRemove.forEach(square => container.removeChild(square));
+  }
+
+  let l = container.offsetWidth / n;
+
+  for (let i = 1; i < (n * n + 1); i++) {
+    const square = document.createElement("div");  
+    square.classList.add("grid");
+    square.style.width = l + "px";
+    square.style.height = l + "px";
+    container.appendChild(square);
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = casualColor();
+    });
+  }
+});
